1681842584aee427d19b84e818492eef520ff843
yazjig <yazjig@mcmaster.ca>
2023-05-26 00:34:09 -0400

    This branch produces birds eye BirdsEyeView cam1: front cam2: right cam3: rear cam4: left

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ from camera_locations import CameraLocations
 from listen_to_image import process_image
 from listen_to_image import perspective_transform
 from attach_camera import AttachCamera
+import threading
 
 client = ConnectToCarla().execute()
 
@@ -14,9 +15,52 @@ spawn_point = spectator.get_transform()
 
 vehicle = AddVehicle(world, spawn_point).execute()
 
-camera1 = AttachCamera(world, vehicle).execute(480, 640, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+w = 320
+h = 240
 
-camera1.listen(lambda image: perspective_transform(image, 480, 640))
+# camera1 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+
+# camera1.listen(lambda image: perspective_transform(1, image, h, w))
+
+# camera2 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.RearLocation, CameraLocations.RearRotation)
+
+# camera2.listen(lambda image: perspective_transform(2, image, h, w))
+
+# camera3 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.RightLocation, CameraLocations.RightRotation)
+
+# camera3.listen(lambda image: perspective_transform(3, image, h, w))
+
+# camera4 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.LeftLocation, CameraLocations.LeftRotation)
+
+# camera4.listen(lambda image: perspective_transform(4, image, h, w))
+
+# Create camera objects
+
+def camera_listen(id, camera):
+    camera.listen(lambda image: perspective_transform(id, image, h, w))
+
+camera1 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+camera2 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.RearLocation, CameraLocations.RearRotation)
+camera3 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.RightLocation, CameraLocations.RightRotation)
+camera4 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.LeftLocation, CameraLocations.LeftRotation)
+
+# Create threads for camera listens
+thread1 = threading.Thread(target=camera_listen, args=(1, camera1))
+thread2 = threading.Thread(target=camera_listen, args=(3, camera2))
+thread3 = threading.Thread(target=camera_listen, args=(2, camera3))
+thread4 = threading.Thread(target=camera_listen, args=(4, camera4))
+
+# Start the threads
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+# Wait for all threads to finish
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
 
 while True:
     world.tick()
--- listen_to_image.py
+++ listen_to_image.py
@@ -1,24 +1,45 @@
 import cv2
 import numpy as np
 
-def process_image(image, im_height, im_width):
+def process_image(id, image, im_height, im_width):
     i = np.array(image.raw_data)
     i2 = i.reshape((im_height, im_width, 4))
     i3 = i2[:, :, :3]
-    cv2.imshow("", i3)
+    cv2.imshow(str(id), i3)
     cv2.waitKey(1)
     return i3/255.0
 
-x1, y1 = 200, 200  # Top-left corner
-x2, y2 = 400, 200  # Top-right corner
-x3, y3 = 600, 400  # Bottom-right corner
-x4, y4 = 0, 400    # Bottom-left corner
+x1, y1 = 100, 100  # Top-left corner
+x2, y2 = 200, 100  # Top-right corner
+x3, y3 = 300, 200  # Bottom-right corner
+x4, y4 = 0, 200    # Bottom-left corner
 
-def perspective_transform(image, im_height, im_width):
+images = [None, None, None, None]
+
+def combine_images(im_height, im_width):
+
+    h = im_height
+    w = im_width
+
+    # Create a canvas to combine the images
+    canvas_width = w * 3
+    canvas_height = h * 3
+    canvas = np.zeros((canvas_height, canvas_width, 4), dtype=np.uint8)
+
+    # Place the images onto the canvas
+    canvas[h//2:h+h//2, 0:w] = images[3] # left
+    canvas[0:h, w:w*2] = images[0] # front
+    canvas[int(h*1.5):int(h*2.5), w:w*2] = images[2] # rear
+    canvas[h//2:h+h//2, w*2:w*3] = images[1] # right
+
+    cv2.imshow("Bird's eye view", canvas)
+    cv2.waitKey(10)
+
+def perspective_transform(id, image, im_height, im_width):
     # Define the source points for the perspective transform
     source_points = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
 
-    offset = 75  # Define an offset to create some space around the transformed image
+    offset = 50  # Define an offset to create some space around the transformed image
     destination_points = np.float32([[offset, offset], [im_width-offset, offset], [im_width-offset, im_height-offset], [offset, im_height-offset]])
 
     # Compute the perspective transform matrix
@@ -28,12 +49,22 @@ def perspective_transform(image, im_height, im_width):
     image_array = np.array(image.raw_data)
 
     # Reshape the image array to the desired dimensions
-    image_reshaped = image_array.reshape((image.height, image.width, 4))
+    image_reshaped = image_array.reshape((im_height, im_width, 4))
 
     # Perform the perspective transform
     transformed_image = cv2.warpPerspective(image_reshaped, perspective_matrix, (im_width, im_height))
 
-    cv2.imshow("", transformed_image)
-    cv2.waitKey(1)
+    # Calculate the rotation matrix
+    rotation_matrix = cv2.getRotationMatrix2D((im_width/2, im_height/2), -(id-1)*90, 1)
 
-    return transformed_image
+    # Apply the rotation to the image
+    rotated_image = cv2.warpAffine(transformed_image, rotation_matrix, (im_width, im_height))
+
+    images[id-1] = rotated_image
+    
+    if all(item is not None for item in images):
+        combine_images(im_height, im_width)
+    #cv2.imshow(str(id), rotated_image)
+    #cv2.waitKey(10)
+
+    return transformed_image, rotated_image

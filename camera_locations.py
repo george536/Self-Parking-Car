5bc78ec99cf4af8d197eec91d3088865eb0083dc
yazjig <yazjig@mcmaster.ca>
2023-05-26 00:34:09 -0400

    This branch produces birds eye BirdsEyeView cam1: front cam2: right cam3: rear cam4: left

--- camera_locations.py
+++ camera_locations.py
@@ -6,7 +6,7 @@ class CameraLocations(Enum):
     FrontRotation = carla.Rotation(pitch=-15, yaw=0, roll=0)
 
     RearLocation = carla.Location(x=-1.5, y=0, z=2)
-    RearRotation = carla.Rotation(pitch=-15, yaw=180, roll=0)
+    RearRotation = carla.Rotation(pitch=-20, yaw=180, roll=0)
 
     RightLocation = carla.Location(x=0, y=1.5, z=2)
     RightRotation = carla.Rotation(pitch=-15, yaw=90, roll=0)
--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ from camera_locations import CameraLocations
 from listen_to_image import process_image
 from listen_to_image import perspective_transform
 from attach_camera import AttachCamera
+import threading
 
 client = ConnectToCarla().execute()
 
@@ -14,9 +15,34 @@ spawn_point = spectator.get_transform()
 
 vehicle = AddVehicle(world, spawn_point).execute()
 
-camera1 = AttachCamera(world, vehicle).execute(480, 640, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+w = 320
+h = 240
 
-camera1.listen(lambda image: perspective_transform(image, 480, 640))
+def camera_listen(id, camera, h, w, offset):
+    camera.listen(lambda image: perspective_transform(id, image, h, w, offset))
+
+camera1 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+camera2 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.RearLocation, CameraLocations.RearRotation)
+camera3 = AttachCamera(world, vehicle).execute(h, w, 140, CameraLocations.RightLocation, CameraLocations.RightRotation)
+camera4 = AttachCamera(world, vehicle).execute(h, w, 140, CameraLocations.LeftLocation, CameraLocations.LeftRotation)
+
+# Create threads for camera listens
+thread1 = threading.Thread(target=camera_listen, args=(1, camera1, h, w, 50))
+thread2 = threading.Thread(target=camera_listen, args=(3, camera2, h, w, 50))
+thread3 = threading.Thread(target=camera_listen, args=(2, camera3, h, w, 70))
+thread4 = threading.Thread(target=camera_listen, args=(4, camera4, h, w, 70))
+
+# Start the threads
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+# Wait for all threads to finish
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
 
 while True:
     world.tick()

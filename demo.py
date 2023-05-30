71a6e3eaa02785e6f1dedcdcdea25bf20a6464b4
yazjig <yazjig@mcmaster.ca>
2023-05-30 00:26:08 -0400

    Tried some changes

--- demo.py
+++ demo.py
@@ -5,7 +5,6 @@ from listen_to_image import process_image
 from listen_to_image import perspective_transform
 from attach_camera import AttachCamera
 import threading
-import threading
 
 client = ConnectToCarla().execute()
 
@@ -16,7 +15,28 @@ spawn_point = spectator.get_transform()
 
 vehicle = AddVehicle(world, spawn_point).execute()
 
-camera1 = AttachCamera(world, vehicle).execute(480, 640, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+w = 320
+h = 240
+
+def camera_listen(id, camera, h, w, offset):
+    camera.listen(lambda image: perspective_transform(id, image, h, w, offset))
+
+camera1 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.FrontLocation, CameraLocations.FrontRotation)
+camera2 = AttachCamera(world, vehicle).execute(h, w, 90, CameraLocations.RearLocation, CameraLocations.RearRotation)
+camera3 = AttachCamera(world, vehicle).execute(h, w, 140, CameraLocations.RightLocation, CameraLocations.RightRotation)
+camera4 = AttachCamera(world, vehicle).execute(h, w, 140, CameraLocations.LeftLocation, CameraLocations.LeftRotation)
+
+# Create threads for camera listens
+thread1 = threading.Thread(target=camera_listen, args=(1, camera1, h, w, 50))
+thread2 = threading.Thread(target=camera_listen, args=(3, camera2, h, w, 50))
+thread3 = threading.Thread(target=camera_listen, args=(2, camera3, h, w, 70))
+thread4 = threading.Thread(target=camera_listen, args=(4, camera4, h, w, 70))
+
+# Start the threads
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
 
 # Wait for all threads to finish
 thread1.join()

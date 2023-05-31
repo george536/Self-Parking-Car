54f33a5bca4798d623f6fbae1c27ebf99e7d1832
Omar Alam <omeralam7@gmail.com>
2023-05-31 19:51:59 -0400

    refactor code and remove unnecessary util functions

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from data_collection.parking_labeller import parking_labeller
 
 
 if __name__ == '__main__':
-    labeller = parking_labeller()
+    labeller = parking_labeller(load_world='Town05')
     labeller.run()
--- utils/connect_to_carla.py
+++ utils/connect_to_carla.py
@@ -6,5 +6,5 @@ class ConnectToCarla(CMD):
     def execute(self):
         # Connect to the CARLA server
         client = carla.Client('localhost', 2000)
-        client.set_timeout(25.0)
+        client.set_timeout(60.0)
         return client

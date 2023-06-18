33a5cec544ddb04fc6f3091afce5024ba356fc6a
yazjig <yazjig@mcmaster.ca>
2023-06-18 16:45:12 -0400

    calibrate to calibrate_BEV

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 from Birds_Eye_View.run import BirdsEyeView
 
 # Use this toold to calibrate the Bird's Eye View
-if "-calibrate" in sys.argv:
+if "-calibrate_BEV" in sys.argv:
     s = BirdsEyeView(True)
     s.start()
     s.join()

a4d052b1aea9b56b09fc4bf76b17d8075ae5da3e
yazjig <yazjig@mcmaster.ca>
2023-09-19 20:06:03 -0400

    Fixed build script

--- ipc_build.ps1
+++ ipc_build.ps1
@@ -1,3 +1,3 @@
-ipc_dependencies_build.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File ipc_dependencies_build.ps1
 
-ipc_grpc_build.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File ipc_grpc_build.ps1

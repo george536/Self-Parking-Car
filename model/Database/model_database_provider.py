f17b212bb9b98bf54eefca3ba65bb2fe1f6dac23
yazjig <yazjig@mcmaster.ca>
2024-06-05 00:27:42 -0400

    Add database table data provider class

---
+++ model/Database/model_database_provider.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class ModelDatbaseProvider:
+    def __init__(self, db_file="model_database.db"):
+        self.conn = sqlite3.connect(db_file)
+
+    def get_parking_spot_corners(self, spot_id):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT x_BL, y_BL, x_UL, y_UL, x_UR, y_UR, x_BR, y_BR FROM Spots WHERE id = ?", (spot_id,))
+        return cursor.fetchone()
+
+    def get_all_in_view_spots(self, image_id):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT in_view_spots FROM Images WHERE id = ?", (image_id,))
+        result = cursor.fetchone()
+        if result:
+            return result[0].split(",")
+        return []
+
+    def get_car_transform(self, image_id):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT x, y, z, roll, pitch, yaw FROM Images WHERE id = ?", (image_id,))
+        return cursor.fetchone()
+    
+    def close(self):
+        self.conn.close()
